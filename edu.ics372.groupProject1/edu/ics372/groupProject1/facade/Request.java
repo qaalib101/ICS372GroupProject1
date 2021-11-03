8ef06fb800ee98cc45256e5a9ff7de5d4f64049d
SweetNaterTater <41494817+SweetNaterTater@users.noreply.github.com>
2021-11-02 20:35:39 -0500

    Update Request.java
    Added some modifiers for Transaction fields

--- edu.ics372.groupProject1/edu/ics372/groupProject1/facade/Request.java
+++ edu.ics372.groupProject1/edu/ics372/groupProject1/facade/Request.java
@@ -2,7 +2,9 @@ package edu.ics372.groupProject1.facade;
 
 public class Request extends DataTransfer {
 	private static Request request;
-
+	private Calendar date, date1, date2;
+	private double total;
+	
 	private Request() {
 
 	}
@@ -18,5 +20,34 @@ public class Request extends DataTransfer {
 		}
 		return request;
 	}
+	
+	/*
+	 * Store more helper methods for printing out transactions
+	 * 
+	 * Such as boolean checks to verify the date range, and setting date1 & date2.
+	 */
 
+	public Calendar getDate() {
+		return date;
+	}
+
+	public Calendar getDate1() {
+		return date1;
+	}
+
+	public void setDate1(Calendar date1) {
+		this.date1 = date1;
+	}
+
+	public Calendar getDate2() {
+		return date2;
+	}
+
+	public void setDate2(Calendar date2) {
+		this.date2 = date2;
+	}
+
+	public double getTotal() {
+		return total;
+	}
 }
