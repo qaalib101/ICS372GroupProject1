0c874560574599dfa7d11d34c56dfd88cac8eb2d
SweetNaterTater <41494817+SweetNaterTater@users.noreply.github.com>
2021-11-03 10:49:04 -0500

    Updated AutomatedTester.java
    Implemented addMember and addProduct. Need to find more information on cart stuff within data transfer to continue

--- edu.ics372.groupProject1/edu/ics372/groupProject1/tests/AutomatedTester.java
+++ edu.ics372.groupProject1/edu/ics372/groupProject1/tests/AutomatedTester.java
@@ -1,16 +1,78 @@
 package edu.ics372.groupProject1.tests;
 
 import edu.ics372.groupProject1.facade.GroceryStore;
+import edu.ics372.groupProject1.entities.Cart;
+import edu.ics372.groupProject1.entities.Member;
+import edu.ics372.groupProject1.entities.Product;
+import edu.ics372.groupProject1.facade.Request;
+import edu.ics372.groupProject1.facade.Result;
 
+/**
+ * This class generates sample automated tests for the library system using
+ * asserts.
+ * 
+ * @author Brahma Dathan
+ *
+ */
 public class AutomatedTester {
 	private GroceryStore store;
+	private String[] names = { "n1", "n2", "n3" };
+	private String[] addresses = { "a1", "a2", "a3" };
+	private String[] phones = { "p1", "p2", "p3" };
+	private Member[] members = new Member[3];
+	private String[] storeItems = { "Apple", "Steak", "Eggs" };
+	private String[] price = { "1.50", "3.25", "6.00" };
+	private String[] reorderLevel = { "11", "12", "13" };
+	private Product[] products = new Product[3];
+	private String[] authors = { "a1", "a2", "a3", "a4", "a5", "a6" };
+	private String[] ids = { "i1", "i2", "i3", "i4", "i5", "i6" };
+	private Cart[] cart = new Cart[6];
 
-	public void testProcessShipment() {
+	/**
+	 * Tests Member creation.
+	 */
+	public void testAddMember() {
+		for (int count = 0; count < members.length; count++) {
+			Request.instance().setMemberAddress(addresses[count]);
+			Request.instance().setMemberName(names[count]);
+			Result result = store.instance().addMember(Request.instance());
+			assert result.getResultCode() == Result.OPERATION_COMPLETED;
+			assert result.getMemberName().equals(names[count]);
+			assert result.getMemberPhone().equals(phones[count]);
+		}
+	}
 
+	public void testAddProduct() {
+		for (int count = 0; count < products.length; count++) {
+			Request.instance().setProductPrice(price[count]);
+			Request.instance().setProductName(storeItems[count]);
+			Request.instance().setProductMinimumReorderLevel(ids[count]);
+			Result result = store.instance().addProduct(Request.instance());
+			assert result.getResultCode() == Result.OPERATION_COMPLETED;
+			assert result.getProductName().equals(storeItems[count]);
+			assert result.getProductPrice().equals(price[count]);
+			assert result.getProductPrice().equals(reorderLevel[count]);
+		}
+	}
+	
+	public void testAddItemToCart() {
+		for(int count = 0; count < cart.length; count++) {
+			Request.instance()
+		}
+	}
+
+	public void testSearchMembership() {
+		Request.instance().setMemberId("M1");
+		// assert
+		// Library.instance().searchMembership(Request.instance()).getMemberId().equals("M1");
+		Request.instance().setMemberId("M4");
+		// assert Library.instance().searchMembership(Request.instance()) == null;
 	}
 
 	public void testAll() {
-		testProcessShipment();
+		testAddMember();
+		testAddProduct();
+		testSearchMembership();
 	}
 
 	public static void main(String[] args) {
