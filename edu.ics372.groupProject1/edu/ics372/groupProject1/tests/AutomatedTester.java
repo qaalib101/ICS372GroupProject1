893b9edabf0c05e6438a38139035099e7e30a53c
hapticfish <jquinmail@gmail.com>
2021-11-09 22:01:44 -0600

    Corrected missing product declaration in GroceryStore.checkOutProduct

--- edu.ics372.groupProject1/edu/ics372/groupProject1/tests/AutomatedTester.java
+++ edu.ics372.groupProject1/edu/ics372/groupProject1/tests/AutomatedTester.java
@@ -30,7 +30,7 @@ public class AutomatedTester {
 	private String[] storeItems = { "Apple", "Steak", "Eggs" };
 	private String[] price = { "1.50", "3.25", "6.00" };
 	private String[] reorderLevel = { "11", "12", "13" };
-	private String[] orderAmount = {"22", "24", "26"};
+	private String[] orderAmount = { "22", "24", "26" };
 	private Product[] products = new Product[3];
 	private String[] ids = { "i1", "i2", "i3", "i4", "i5", "i6" };
 	private Cart[] cart = new Cart[6];
@@ -63,35 +63,35 @@ public class AutomatedTester {
 	}
 
 	public void generatateTestData() {
-		// create memebers
-		store.addMember("Solaire", "1271 Walton St. Saint Paul, MN,55668", "1234567788", "02/01/1955", 0);
-		store.addMember("Gwyndolin", "123 Easy St. NewYork, NY, 11225", "1234568855", "01/22/05", 0);
-		store.addMember("Andre", "123 Walketon Ave. Stockton, NJ, 44589", "5564498756", "02/22/1926", 0);
-		store.addMember("Yuria", "456 Shelly Ave., CastleVania, KS, 11122", "5556668888", "01/01/2001", 0);
-		store.addMember("Eldritch", "000 Irithyll lane, Irithyll, AK, 00000", "0000001111", "09/21/1311", 0);
-
-		// create Products
-		store.addProduct("Milk", 3.75, 20);
-		store.addProduct("Eggs", 1.99, 20);
-		store.addProduct("Wheat Bread", 2.76, 20);
-		store.addProduct("Orange Juice", 3.26, 20);
-		store.addProduct("Iced Tea", 2.25, 20);
-		store.addProduct("Apple", 1.00, 20);
-		store.addProduct("Strawberrys", 3.11, 30);
-		store.addProduct("Bananas", 1.01, 15);
-		store.addProduct("Avacado", 1.33, 15);
-		store.addProduct("Chicken Noodle Soup Canned", 1.45, 15);
-		store.addProduct("Hawaiian Buns", 6.55, 10);
-		store.addProduct("Turkey Breasts", 5.55, 10);
-		store.addProduct("Roast Beef", 4.25, 15);
-		store.addProduct("Brown Rice", .88, 20);
-		store.addProduct("Tomato Sause", 1.13, 20);
-		store.addProduct("BBQ Sause", 2.22, 15);
-		store.addProduct("Shrimp", 13.99, 16);
-		store.addProduct("Butter", 2.45, 25);
-		store.addProduct("Hummus", 3.69, 14);
-		store.addProduct("Chocolate Bar", 1.75, 30);
-		store.addProduct("Spinach", .33, 15);
+//		// create memebers
+//		store.addMember("Solaire", "1271 Walton St. Saint Paul, MN,55668", "1234567788", "02/01/1955", 0);
+//		store.addMember("Gwyndolin", "123 Easy St. NewYork, NY, 11225", "1234568855", "01/22/05", 0);
+//		store.addMember("Andre", "123 Walketon Ave. Stockton, NJ, 44589", "5564498756", "02/22/1926", 0);
+//		store.addMember("Yuria", "456 Shelly Ave., CastleVania, KS, 11122", "5556668888", "01/01/2001", 0);
+//		store.addMember("Eldritch", "000 Irithyll lane, Irithyll, AK, 00000", "0000001111", "09/21/1311", 0);
+//
+//		// create Products
+//		store.addProduct("Milk", 3.75, 20);
+//		store.addProduct("Eggs", 1.99, 20);
+//		store.addProduct("Wheat Bread", 2.76, 20);
+//		store.addProduct("Orange Juice", 3.26, 20);
+//		store.addProduct("Iced Tea", 2.25, 20);
+//		store.addProduct("Apple", 1.00, 20);
+//		store.addProduct("Strawberrys", 3.11, 30);
+//		store.addProduct("Bananas", 1.01, 15);
+//		store.addProduct("Avacado", 1.33, 15);
+//		store.addProduct("Chicken Noodle Soup Canned", 1.45, 15);
+//		store.addProduct("Hawaiian Buns", 6.55, 10);
+//		store.addProduct("Turkey Breasts", 5.55, 10);
+//		store.addProduct("Roast Beef", 4.25, 15);
+//		store.addProduct("Brown Rice", .88, 20);
+//		store.addProduct("Tomato Sause", 1.13, 20);
+//		store.addProduct("BBQ Sause", 2.22, 15);
+//		store.addProduct("Shrimp", 13.99, 16);
+//		store.addProduct("Butter", 2.45, 25);
+//		store.addProduct("Hummus", 3.69, 14);
+//		store.addProduct("Chocolate Bar", 1.75, 30);
+//		store.addProduct("Spinach", .33, 15);
 
 	}
 
@@ -169,6 +169,7 @@ public class AutomatedTester {
 			++id;
 		}
 
+		
 	public void testAll() {
 		testCheckOutCart();
 		testAddMember();
@@ -177,10 +178,11 @@ public class AutomatedTester {
 		testListMembers();
 		testListProducts();
 		testListOrders();
-	}
+	}//end testAll
+	}// end testListOrder
 
 	public static void main(String[] args) {
 
 		new AutomatedTester().testAll();
-	}
-}
+	}// end Main
+}// end AutomatedTester
