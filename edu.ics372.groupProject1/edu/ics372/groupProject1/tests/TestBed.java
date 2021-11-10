fa7e2c18620e9f4ab7b73fa765c8fa1c9fd697d6
hapticfish <jquinmail@gmail.com>
2021-11-09 23:44:41 -0600

    Final Push

--- edu.ics372.groupProject1/edu/ics372/groupProject1/tests/TestBed.java
+++ edu.ics372.groupProject1/edu/ics372/groupProject1/tests/TestBed.java
@@ -1,8 +1,11 @@
 package edu.ics372.groupProject1.tests;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
+import edu.ics372.groupProject1.entities.CartItem;
 import edu.ics372.groupProject1.entities.Member;
 import edu.ics372.groupProject1.entities.Product;
 import edu.ics372.groupProject1.facade.GroceryStore;
@@ -30,6 +33,7 @@ public class TestBed {
 	private String[] storeItems = { "product1", "product2", "product3", "product4", "product5", "product6", "product7",
 			"product8", "product9", "product10", "product11", "product12", "product13", "product14", "product15",
 			"product16", "product17", "product18", "product19", "product20" };
+	private List<CartItem> cartItems = new ArrayList<CartItem>();
 	private double[] price = { 1.50, 3.25, 6.00, 3.26, 2.25, 1.00, 3.11, 1.01, 1.33, 1.45, 6.55, 5.55, 4.25, 0.88, 1.13,
 			2.22, 13.99, 2.45, 1.75, 3.69 };
 	private int[] reorderLevel = { 20, 20, 20, 20, 20, 30, 15, 15, 15, 10, 10, 15, 20, 20, 15, 16, 25, 14, 30, 15 };
@@ -106,8 +110,7 @@ public class TestBed {
 	}
 
 	private void TestCheckOutCart() {
-		GeneratateTestData();
-		UI.retrieveProduct();
+
 	}
 
 	private void TestProcessShipment() {
