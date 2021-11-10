bed2dd315ced7ab28cf85f56091d3bbbd30b4d58
SweetNaterTater <41494817+SweetNaterTater@users.noreply.github.com>
2021-11-09 18:46:57 -0600

    Update Cart.java

--- edu.ics372.groupProject1/edu/ics372/groupProject1/entities/Cart.java
+++ edu.ics372.groupProject1/edu/ics372/groupProject1/entities/Cart.java
@@ -1,6 +1,7 @@
 package edu.ics372.groupProject1.entities;
 
 import java.io.Serializable;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,34 +10,45 @@ import edu.ics372.groupProject1.collections.Inventory;
 /**
  * @author jquin
  *
- *         Cart Class Holds a list of products to be checked out by the customer
+ *         Cart Class Holds a list of products to be checked out by the
+ *         customer.
  *
  */
 public class Cart implements Serializable {
 	private double totalPrice;
 	private List<CartItem> cartItems;
+	private List<Product> reorderList;
+	DecimalFormat df = new DecimalFormat("0.00");
 
 	public Cart() {
 		super();
 		this.totalPrice = 0;
 		this.cartItems = new ArrayList<CartItem>();
+		this.reorderList = new ArrayList<Product>();
 	}
 
 	/**
-	 * calculateCartTotal Method Calculates and returns the running cart total as
-	 * double.
-	 *
+	 * calculateCartTotal calculates cart total for current cart instance.
+	 * 
+	 * @param List<CartItem>
+	 * @return double
+	 * 
 	 */
-
 	public double calculateCartTotal(List<CartItem> cartItems) {
 
 		this.totalPrice = cartItems.stream().mapToDouble(i -> i.getTotalItemPrice()).sum();
 		return totalPrice;
 	}
 
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
 	/**
-	 * addCartItem Method Adds CartItem to the Cart ArrayList returns True if
-	 * successful, False if failed or exception
+	 * addCartItem method adds CartItem to the Cart ArrayList
+	 *
+	 * @param CartItem
+	 * @return boolean
 	 *
 	 */
 	public boolean addCartItem(CartItem cartItem) {
@@ -46,6 +58,28 @@ public class Cart implements Serializable {
 		} catch (Exception e) {
 			return false;
 		}
+	}
+
+	public List<Product> getReorderList() {
+		return reorderList;
+	}
+
+	/**
+	 * addToReorderList Method Adds Products that have been checked out that have
+	 * broken the reorder threshold.
+	 * 
+	 * @param Product
+	 * @return boolean
+	 *
+	 */
+	public boolean addToReorderList(Product product) {
+		try {
+			reorderList.add(product);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+
 	}
 
 	/**
@@ -64,6 +98,20 @@ public class Cart implements Serializable {
 	 */
 	public List<CartItem> getCartItems() {
 		return this.cartItems;
+	}
+
+	/**
+	 * printCartTotal prints the total of all cart items per specified requirements.
+	 * 
+	 * @param N/A
+	 * @return void
+	 * 
+	 */
+	public void printCartTotal() {
+		String total = "Grand Total";
+		String totalString = String.format("%30s                      $%6s", total, df.format(getTotalPrice()));
+		System.out.println("___________________________________________________________");
+		System.out.println(totalString);
 	}
 
 }
