7efb10dc1dc8733ceedd20401ef0aff91305da61
The-Ninya <55476384+The-Ninya@users.noreply.github.com>
2021-11-09 18:17:58 -0600

    Update AutomatedTester.java

--- edu.ics372.groupProject1/edu/ics372/groupProject1/tests/AutomatedTester.java
+++ edu.ics372.groupProject1/edu/ics372/groupProject1/tests/AutomatedTester.java
@@ -1,17 +1,12 @@
 package edu.ics372.groupProject1.tests;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import edu.ics372.groupProject1.facade.Result;
-import edu.ics372.groupProject1.collections.MemberList;
-import edu.ics372.groupProject1.collections.Inventory;
 import edu.ics372.groupProject1.entities.Cart;
 import edu.ics372.groupProject1.entities.Member;
-import edu.ics372.groupProject1.entities.Order;
 import edu.ics372.groupProject1.entities.Product;
 import edu.ics372.groupProject1.facade.GroceryStore;
-import edu.ics372.groupProject1.ui.UserInterface;
 import edu.ics372.groupProject1.facade.Request;
+import edu.ics372.groupProject1.facade.Result;
+import edu.ics372.groupProject1.ui.UserInterface;
 
 /**
  * This class generates automated tests for the library system using asserts.
@@ -33,35 +28,8 @@ public class AutomatedTester {
 	private String[] ids = { "i1", "i2", "i3", "i4", "i5", "i6" };
 	private Cart[] cart = new Cart[6];
 
-	/**
-	 * Tests Member creation.
-	 */
-	public void testAddMember() {
-		for (int count = 0; count < members.length; count++) {
-			Request.instance().setMemberAddress(addresses[count]);
-			Request.instance().setMemberName(names[count]);
-			Result result = store.instance().addMember(Request.instance());
-			assert result.getResultCode() == Result.OPERATION_COMPLETED;
-			assert result.getMemberName().equals(names[count]);
-			assert result.getMemberPhone().equals(phones[count]);
-		}
-	}
-
-	public void testAddProduct() {
-		for (int count = 0; count < products.length; count++) {
-			Request.instance().setProductCurrentPrice(price[count]);
-			Request.instance().setProductName(storeItems[count]);
-			Request.instance().setProductMinimumReorderLevel(ids[count]);
-			Result result = store.instance().addProduct(Request.instance());
-			assert result.getResultCode() == Result.OPERATION_COMPLETED;
-			assert result.getProductName().equals(storeItems[count]);
-			assert result.getProductCurrentPrice().equals(price[count]);
-			assert result.getProductMinimumReorderLevel().equals(reorderLevel[count]);
-		}
-	}
-
-	public void generatateTestData() {
-		// create memebers
+	public void GeneratateTestData() {
+		// create members
 		store.addMember("Solaire", "1271 Walton St. Saint Paul, MN,55668", "1234567788", "02/01/1955", 0);
 		store.addMember("Gwyndolin", "123 Easy St. NewYork, NY, 11225", "1234568855", "01/22/05", 0);
 		store.addMember("Andre", "123 Walketon Ave. Stockton, NJ, 44589", "5564498756", "02/22/1926", 0);
@@ -93,100 +61,132 @@ public class AutomatedTester {
 
 	}
 
-	private void testCheckOutCart() {
-		generatateTestData();
+	/**
+	 * Tests Member creation.
+	 */
+	public void TestAddMember() {
+		for (int count = 0; count < members.length; count++) {
+			Request.instance().setMemberAddress(addresses[count]);
+			Request.instance().setMemberName(names[count]);
+			Result result = store.instance().addMember(Request.instance());
+			assert result.getResultCode() == Result.OPERATION_COMPLETED;
+			assert result.getMemberName().equals(names[count]);
+			assert result.getMemberPhone().equals(phones[count]);
+		}
+	}
+
+	public void TestAddProduct() {
+		for (int count = 0; count < products.length; count++) {
+			Request.instance().setProductCurrentPrice(price[count]);
+			Request.instance().setProductName(storeItems[count]);
+			Request.instance().setProductMinimumReorderLevel(ids[count]);
+			Result result = store.instance().addProduct(Request.instance());
+			assert result.getResultCode() == Result.OPERATION_COMPLETED;
+			assert result.getProductName().equals(storeItems[count]);
+			assert result.getProductCurrentPrice().equals(price[count]);
+			assert result.getProductMinimumReorderLevel().equals(reorderLevel[count]);
+		}
+	}
+
+	private void TestCheckOutCart() {
+		GeneratateTestData();
 		UI.retrieveProduct();
 		UI.checkoutCart();
-
-	public void testSearchMembership() {
-		Request.instance().setMemberId("M1");
-		// assert
-		// Library.instance().searchMembership(Request.instance()).getMemberId().equals("M1");
-		Request.instance().setMemberId("M4");
-		// assert Library.instance().searchMembership(Request.instance()) == null;
-	}
-	
-	/**
-	 * Tests a list of Members
-	 */
-
-	/**
-	 * Tests a list of Members
-	 */
-	public void testListMembers() {
-		Iterator<Result> testMembers = store.listMembers();
-
-		while (testMembers.hasNext()) {
-			int i = 0;
-			Result actual = testMembers.next();
-			MemberList expected = MemberList.getInstance();
-			assert actual.getMemberId().equals(i);
-			assert actual.getMemberName().equals(names[i]);
-			assert actual.getMemberAddress().equals(addresses[i]);
-			i++;
-
-		}
 	}
 
-	/**
-	 * Tests a list of Products
-	 */
-
-	public void testListProducts() {
-		Iterator<Result> testProducts = store.listProducts();
-
-		while (testProducts.hasNext()) {
-			int i = 0;
-			Result actual = testProducts.next();
-			Inventory expected = Inventory.getInstance();
-			assert actual.getProductId().equals(i);
-			assert actual.getProductName().equals(names[i]);
-			assert actual.getProductCurrentPrice().equals(price[i]);
-			assert actual.getProductMinimumReorderLevel().equals(reorderLevel[i]);
-			i++;
-
-		}
+	private void TestProcessShipment() {
+		// TODO Auto-generated method stub
 
 	}
 
-	/**
-	 *Test a list of outstanding orders 
-	 */
+	private void TestPrintTransactions() {
+		// TODO Auto-generated method stub
 
-	public void testListOrders() {
-		Iterator<Result> testOrders = store.listOrders();
-		Order order1 = new Order("P1", "Apple", 22);
-		Order order2 = new Order("P2", "Steak", 24);
-		Order order3 = new Order("P3", "Eggs", 26);
-		LinkedList<Order> orderList = new LinkedList<Order>();
-		orderList.add(order1);
-		orderList.add(order2);
-		orderList.add(order3);
+	}
 
-		while (testOrders.hasNext()) {
-			int i = 0;
-			Result actual = testOrders.next();
-			Order expected = orderList.get(i);
-			assert expected.getProductId().equals(actual.getOrderProductId());
-			assert expected.getProductName().equals(actual.getOrderProductName());
-			assert expected.getAmountOrdered().equals(actual.getAmountOrdered());
-			i++;
-		}
+//	public void TestSearchMembership() {
+//		Request.instance().setMemberId("M1");
+//		// assert
+//		// Library.instance().searchMembership(Request.instance()).getMemberId().equals("M1");
+//		Request.instance().setMemberId("M4");
+//		// assert Library.instance().searchMembership(Request.instance()) == null;
+//	}
 
+//	/**
+//	 * Tests a list of Members
+//	 */
+//	public void TestListMembers() {
+//		Iterator<Result> testMembers = store.listMembers();
+//
+//		while (testMembers.hasNext()) {
+//			int i = 0;
+//			Result actual = testMembers.next();
+//			MemberList expected = MemberList.getInstance();
+//			assert actual.getMemberId().equals(i);
+//			assert actual.getMemberName().equals(names[i]);
+//			assert actual.getMemberAddress().equals(addresses[i]);
+//			i++;
+//
+//		}
+//	}
 
-	public void testAll() {
-		testProcessShipment();
-		testCheckOutCart();
-		testAddMember();
-		testAddProduct();
-		testSearchMembership();
-		testListMembers();
-		testListProducts();
-		testListOrders();
+//	/**
+//	 * Tests a list of Products
+//	 */
+//	public void TestListProducts() {
+//		Iterator<Result> testProducts = store.listProducts();
+//
+//		while (testProducts.hasNext()) {
+//			int i = 0;
+//			Result actual = testProducts.next();
+//			Inventory expected = Inventory.getInstance();
+//			assert actual.getProductId().equals(i);
+//			assert actual.getProductName().equals(names[i]);
+//			assert actual.getProductCurrentPrice().equals(price[i]);
+//			assert actual.getProductMinimumReorderLevel().equals(reorderLevel[i]);
+//			i++;
+//
+//		}
+//
+//	}
+
+//	/**
+//	 *Test a list of outstanding orders 
+//	 */
+//	public void TestListOrders() {
+//		Iterator<Result> testOrders = store.listOrders();
+//		Order order1 = new Order("P1", "Apple", 22);
+//		Order order2 = new Order("P2", "Steak", 24);
+//		Order order3 = new Order("P3", "Eggs", 26);
+//		LinkedList<Order> orderList = new LinkedList<Order>();
+//		orderList.add(order1);
+//		orderList.add(order2);
+//		orderList.add(order3);
+//		while (testOrders.hasNext()) {
+//			int i = 0;
+//			Result actual = testOrders.next();
+//			Order expected = orderList.get(i);
+//			assert expected.getProductId().equals(actual.getOrderProductId());
+//			assert expected.getProductName().equals(actual.getOrderProductName());
+//			assert expected.getAmountOrdered().equals(actual.getAmountOrdered());
+//			i++;
+//		}
+
+	public void TestAll() {
+		TestAddMember();
+		TestAddProduct();
+		TestCheckOutCart();
+		TestProcessShipment();
+		TestPrintTransactions();
+
+//		testSearchMembership();
+//		testListMembers();
+//		testListProducts();
+//		testListOrders();
 	}
 
 	public static void main(String[] args) {
 
-		new AutomatedTester().testAll();
+		new AutomatedTester().TestAll();
 	}
 }
