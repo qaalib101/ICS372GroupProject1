31cb6e91a91c3cbb3329d83acca471aacaf908c4
SweetNaterTater <41494817+SweetNaterTater@users.noreply.github.com>
2021-11-09 19:05:59 -0600

    Update GroceryStore.java

--- edu.ics372.groupProject1/edu/ics372/groupProject1/facade/GroceryStore.java
+++ edu.ics372.groupProject1/edu/ics372/groupProject1/facade/GroceryStore.java
@@ -1,17 +1,21 @@
-package edu.ics372.groupProject1.facade;
+package groupProject1.facade;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 import edu.ics372.groupProject1.collections.Inventory;
 import edu.ics372.groupProject1.collections.MemberList;
 import edu.ics372.groupProject1.collections.OrderList;
+import edu.ics372.groupProject1.entities.CartItem;
 import edu.ics372.groupProject1.entities.Member;
 import edu.ics372.groupProject1.entities.Order;
 import edu.ics372.groupProject1.entities.Product;
+import edu.ics372.groupProject1.entities.Transaction;
 import edu.ics372.groupProject1.iterators.SafeMemberIterator;
 import edu.ics372.groupProject1.iterators.SafeOrderIterator;
 import edu.ics372.groupProject1.iterators.SafeProductIterator;
@@ -85,27 +89,159 @@ public class GroceryStore {
 				request.getProductMinimumReorderLevel());
 		boolean addProduct = inventory.insertProduct(newProduct);
 		if (addProduct) {
-			result.setResultCode(result.OPERATION_COMPLETED);
+			result.setResultCode(Result.OPERATION_COMPLETED);
 			result.setProductFields(newProduct);
 		}
-		// I'm a little confused about how ordering will be implemented
-//		Order(name, price, reorderLevel * 2);
 		return result;
 	}
 
-	public Result checkoutCart(Request request) {
-		Result result = new Result();
-		return result;
-	}
+	/**
+	 * CODE FOR CHECKING OUT CART
+	 */
 
-	public Result retrieveProduct(Request request) {
+	/**
+	 * Searches for a given member
+	 * 
+	 * @param memberId id of the member
+	 * @return true iff the member is in the member list collection
+	 */
+	public Result searchMembership(Request request) {
 		Result result = new Result();
-		Product product = inventory.searchByName(request.getProductName());
-		if (product != null) {
-			result.setResultCode(Result.OPERATION_COMPLETED);
-			result.setProductFields(product);
+		Member member = members.search(request.getMemberId());
+		if (member == null) {
+			result.setResultCode(Result.NO_SUCH_MEMBER);
 		} else {
-			result.setResultCode(Result.OPERATION_FAILED);
+			result.setResultCode(Result.OPERATION_COMPLETED);
+			result.setMemberFields(member);
+		}
+		return result;
+	}
+
+	/*
+	 * Checks out product. Adds item to the cart, collects the quantity from the
+	 * cartItem updates the inventory level and if necessary reorders the item.
+	 * 
+	 * @param Request
+	 * 
+	 * @return Result
+	 * 
+	 */
+	public Result checkOutProduct(Request request) {
+		Result result = new Result();
+		String productId = request.getProductId();
+		String memberId = request.getMemberId();
+		Member member = members.search(memberId);
+		Product product = inventory.search(productId);
+		if (product == null) {
+			result.setResultCode(Result.PRODUCT_NOT_FOUND);
+			return result;
+		} else {
+			int productCartQuantity = Integer.parseInt(request.getProductCartQuantity());
+			CartItem cartItem = new CartItem(product, productCartQuantity);
+			if (!member.getCart().addCartItem(cartItem)) {
+				result.setResultCode(Result.OPERATION_FAILED);
+			}
+			result.setResultCode(Result.OPERATION_COMPLETED);
+			product.decrementQuantity(productCartQuantity);
+			if (product.autoRestock() && orders.search(productId) == null) { // Null pointer exception is occuring
+				member.getCart().addToReorderList(product);
+				Order newOrder = new Order(productId, product.getName(), Integer.parseInt(product.getReorderLevel()));
+				orders.insertOrder(newOrder);
+				buildReorderList(request);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Builds StringBuilder list of products to be reordered during checkout
+	 * process.
+	 * 
+	 * @param Request
+	 * @return void
+	 * 
+	 */
+	private void buildReorderList(Request request) {
+		String memberId = request.getMemberId();
+		Member member = members.search(memberId);
+		StringBuilder productsToReorder = new StringBuilder("");
+		for (int index = 0; index < member.getCart().getReorderList().size(); index++) {
+			productsToReorder.append(member.getCart().getReorderList().get(index).getName() + "\n");
+		}
+		request.setProductsToBeReordered(productsToReorder);
+	}
+
+	/*
+	 * Calculates carts total
+	 * 
+	 * @param Request
+	 * 
+	 * @return Result
+	 * 
+	 */
+	public Result calculateCartTotalPrice(Request request) {
+		Result result = new Result();
+		String memberId = request.getMemberId();
+		Member member = members.search(memberId);
+		member.getCart().calculateCartTotal(member.getCart().getCartItems());
+		request.setCartTotalPrice(String.valueOf(member.getCart().getTotalPrice()));
+		result.setResultCode(Result.OPERATION_COMPLETED);
+		return result;
+
+	}
+
+	/*
+	 * Prints cart checkout message
+	 * 
+	 * @param Request
+	 * 
+	 * @return void
+	 * 
+	 */
+	public void printCheckOut(Request request) {
+		String productName = "Product Name";
+		String quantity = "Quantity";
+		String unitPrice = "Unit Price";
+		String total = "Total";
+		String header = String.format("%30s %8s %11s   %5s ", productName, quantity, unitPrice, total);
+
+		String itemDetails = "";
+		String memberId = request.getMemberId();
+		Member member = members.search(memberId);
+		System.out.println(header);
+		for (int index = 0; index < member.getCart().getCartItems().size(); index++) {
+			itemDetails = member.getCart().getCartItems().get(index).printItemDetails();
+			System.out.println(itemDetails);
+		}
+		member.getCart().printCartTotal();
+		member.addTransaction(new Transaction((Double.parseDouble(request.getCartTotalPrice()))));// ADDED TRANSACTION
+																									// TO MEMBER's
+																									// transactions
+	}
+
+	/**
+	 * CODE FOR CHECKING OUT CART
+	 */
+
+	/**
+	 * Retrieves product info by name
+	 * 
+	 * @param Request
+	 * 
+	 * @return Result
+	 */
+	public Result retrieveProductInfo(Request request) {
+		Result result = new Result();
+		String productName = request.getProductName();
+		Product product = inventory.searchByName(productName);
+
+		if (product != null) {
+			request.setProductId(product.getId());
+			request.setProductPrice(product.getPrice());
+			request.setProductQuantity(product.getQuantity());
+			result.setResultCode(Result.OPERATION_COMPLETED);
+		} else {
+			result.setResultCode(Result.PRODUCT_NOT_FOUND);
 		}
 		return result;
 	}
@@ -148,6 +284,23 @@ public class GroceryStore {
 		return result;
 	}
 
+	/*
+	 * This method calls on addTransaction from the Member class to add a
+	 * transaction to a given Member
+	 * 
+	 * @return void
+	 */
+	public void addTransactionToMember(Request request) {
+		Member member = members.search(request.getMemberId());
+		member.addTransaction(new Transaction(Double.parseDouble(request.getCartTotalPrice())));
+	}
+
+	/*
+	 * Returns an iterator to Member. The iterator returned is a safe iterator, and
+	 * gets all the transactions of that member between a start and end date range.
+	 * 
+	 * @return void
+	 */
 	public Iterator<Result> getTransactions(Request request) {
 		Member member = members.search(request.getMemberId());
 		if (member == null) {
@@ -199,6 +352,7 @@ public class GroceryStore {
 			FileOutputStream file = new FileOutputStream("CooperativeData");
 			ObjectOutputStream output = new ObjectOutputStream(file);
 			output.writeObject(store);
+			Member.save(output);
 			file.close();
 			return true;
 		} catch (IOException ioe) {
@@ -217,7 +371,7 @@ public class GroceryStore {
 			FileInputStream file = new FileInputStream("GroceryStoreData");
 			ObjectInputStream input = new ObjectInputStream(file);
 			store = (GroceryStore) input.readObject();
-//			Member.retrieve(input);
+			Member.retrieve(input);
 			return store;
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
